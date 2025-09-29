ea22099bf3fb6184cdf2700839b189da50da248f
Dhruv <informdhruv@gmail.com>
2025-09-29 00:18:12 -0400

    basic mcp server examples

---
+++ fastapi_example/math_server.py
@@ -0,0 +1,8 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(name="MathServer", stateless_http=True)
+
+
+@mcp.tool(description="A simple add tool")
+def add_two(n1: int, n2: int) -> int:
+    return n1 + n2
